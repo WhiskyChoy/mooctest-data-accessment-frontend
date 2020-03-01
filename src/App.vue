ae56256497d4add9616d14ecf04e66f24941378c
LAPTOP-PA95VDSS\Admin <1261249659@qq.com>
2020-03-01 17:06:36 +0800

    use pug

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,36 @@
-<template lang="pug">
-    div#app
-        div#nav
-            router-link(to="/") Home
-            | |
-            router-link(to="/about") About
-        router-view
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
-<style lang="less">
-    #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-    }
+<script>
+import HelloWorld from './components/HelloWorld.vue'
 
-    #nav {
-        padding: 30px;
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
 
-        a {
-            font-weight: bold;
-            color: #2c3e50;
-
-            &.router-link-exact-active {
-                color: #42b983;
-            }
-        }
-    }
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
