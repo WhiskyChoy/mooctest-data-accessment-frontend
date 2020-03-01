a425060867686dba11d7b80900ca1f8abc92bd6c
LAPTOP-PA95VDSS\Admin <1261249659@qq.com>
2020-03-02 01:02:06 +0800

    加上过渡进度条

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,61 @@
 <template lang="pug">
     div#app
-        div#nav
-            router-link(to="/") Home
-            | |
-            router-link(to="/about") About
+        header#nav
+            div.link-container
+                router-link(to="/")
+                    img.logo-lg(src="./assets/logo_200x75_1.png")
+            div.link-container
+                router-link(to="/about") 数据质量测评系统
         router-view
 </template>
 
 <style lang="less">
+    @navHeight: 50px;
+
     #app {
         font-family: Avenir, Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
         color: #2c3e50;
-    }
+        margin-top: @navHeight;
 
-    #nav {
-        padding: 30px;
+        #nav {
+            height: @navHeight;
+            position: fixed;
+            top: 0;
+            left: 0;
+            right: 0;
+            display: flex;
+            background-color: dodgerblue;
 
-        a {
-            font-weight: bold;
-            color: #2c3e50;
+            align-items: center;
 
-            &.router-link-exact-active {
-                color: #42b983;
+            .logo-lg {
+                height: @navHeight;
+                object-fit: fill;
             }
+
+            .link-container{
+                width: 200px;
+                height: 100%;
+                display: flex;
+                align-items: center;
+
+                &:hover{
+                    background-color: #23527c;;
+                }
+
+                a {
+                    font-weight: bold;
+                    color: #fff;
+                    font-size: 20px;
+                    width: 100%;
+                    text-decoration: none;
+                }
+            };
+
+
         }
     }
 </style>
