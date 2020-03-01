c05888018afd334b55f22f103bbce31b2d11ac91
LAPTOP-PA95VDSS\Admin <1261249659@qq.com>
2020-03-01 20:06:27 +0800

    use pug

--- src/router/index.js
+++ src/router/index.js
@@ -5,25 +5,29 @@ import Home from '../views/Home.vue'
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+        path: '*',
+        component: () => import(/* webpackChunkName: "about" */ '../views/NotFound.vue')
+    },
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    }
 ];
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 });
 
 export default router
---
+++ src/views/NotFound.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+    div.not-founded
+        h1 您访问的页面不存在
+</template>
+
+<script>
+    export default {
+        name: "NotFound"
+    }
+</script>
+
+<style scoped lang="less">
+
+</style>
