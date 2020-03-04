cccac7f67de7b38b892c66bdda1c001b5231278f
LAPTOP-PA95VDSS\Admin <1261249659@qq.com>
2020-03-04 19:51:12 +0800

    add api description

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,11 @@
 <template lang="pug">
     div.home
-        HelloWorld(msg="Welcome to the home page!")
+        h1 Welcome to the home page!
 </template>
 
 <script>
-    // @ is an alias to /src
-    import HelloWorld from '@/components/HelloWorld.vue'
 
     export default {
         name: 'Home',
-        components: {
-            HelloWorld
-        }
     }
 </script>
