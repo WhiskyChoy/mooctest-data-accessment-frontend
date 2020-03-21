067e4ee2bdb190c9338b209c3563c69215672d77
LAPTOP-PA95VDSS\Admin <1261249659@qq.com>
2020-03-22 00:44:52 +0800

    close history mode

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     /* 部署生产环境和开发环境下的URL：可对当前环境进行区分，baseUrl 从 Vue CLI 3.3 起已弃用，要使用publicPath */
-    // publicPath: process.env.NODE_ENV === 'production' ? '/production-sub-path/' : '/',
+    publicPath: process.env.NODE_ENV === 'debug' ? '/mooctest-data-accessment-frontend/' : '/',
     /* 输出文件目录：在npm run build时，生成文件的目录名称 */
     outputDir: 'dist',
     /* 放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录 */
